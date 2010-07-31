e42b47966d52ee1574bf269f31d87390f9897da9
martin f. krafft <madduck@madduck.net>
2010-07-31 16:22:59 +0200

    migrate rcs class to debian::rcs

---
+++ manifests/rcs.pp
@@ -0,0 +1,53 @@
+# puppet module: debian
+#         class: debian::rcs
+#
+# Copyright © 2010 martin f. krafft <madduck@madduck.net>
+# Released under the terms of the Artistic Licence 2.0
+#
+class debian::rcs {
+
+    $debian_rcs_tmptime = $debian_rcs_tmptime ? {
+        ""      => 7,
+        default => $debian_rcs_tmptime
+    }
+    $debian_rcs_sulogin = $debian_rcs_sulogin ? {
+        ""      => false,
+        default => $debian_rcs_sulogin
+    }
+    $debian_rcs_delaylogin = $debian_rcs_delaylogin ? {
+        ""      => true,
+        default => $debian_rcs_delaylogin
+    }
+    $debian_rcs_utc = $debian_rcs_utc ? {
+        ""      => true,
+        default => $debian_rcs_utc
+    }
+    $debian_rcs_verbose = $debian_rcs_verbose ? {
+        ""      => false,
+        default => $debian_rcs_verbose
+    }
+    $debian_rcs_fsckfix = $debian_rcs_fsckfix ? {
+        ""      => false,
+        default => $debian_rcs_fsckfix
+    }
+    $debian_rcs_ramrun = $debian_rcs_ramrun ? {
+        ""      => false,
+        default => $debian_rcs_ramrun
+    }
+    $debian_rcs_ramlock = $debian_rcs_ramlock ? {
+        ""      => false,
+        default => $debian_rcs_ramlock
+    }
+    $debian_rcs_asyncmountnfs = $debian_rcs_asyncmountnfs ? {
+        ""      => false,
+        default => $debian_rcs_asyncmountnfs
+    }
+
+    file { "/etc/default/rcS":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        content => template("debian/rcS")
+    }
+}
