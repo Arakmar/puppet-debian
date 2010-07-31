b3a1cd681b3a4f9d2cfa918661d8eb1ec2556181
martin f. krafft <madduck@madduck.net>
2010-07-31 16:24:51 +0200

    add debian class to include all

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# puppet module: debian
+#         class: debian
+#
+# Copyright © 2010 martin f. krafft <madduck@madduck.net>
+# Released under the terms of the Artistic Licence 2.0
+#
+class debian {
+    include debian::rcs
+}
