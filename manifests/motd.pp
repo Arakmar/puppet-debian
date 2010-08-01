d3efa946eae046c0a204b5e29d8d43cd8595ec70
martin f. krafft <madduck@madduck.net>
2010-08-01 09:55:34 +0200

    integrate motd handling

---
+++ manifests/motd.pp
@@ -0,0 +1,52 @@
+# puppet module: debian
+#         class: debian::motd
+#
+# Copyright © 2010 martin f. krafft <madduck@madduck.net>
+# Released under the terms of the Artistic Licence 2.0
+#
+class debian::motd::files {
+
+    file { "/etc/motd.tail":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 444,
+        content => template("debian/motd.tail"),
+    }
+
+    file { "/etc/motd":
+        ensure  => "/var/run/motd",
+                require => File["/etc/motd.tail"],
+    }
+}
+
+class debian::motd::update {
+
+    # from /etc/init.d/bootmisc.sh
+    # see http://bugs.debian.org/437176
+    exec { "/var/run/motd":
+        cwd         => "/var/run",
+        path        => "/usr/bin:/bin",
+        command     => "sh -c '{ uname -snrvm; cat /etc/motd.tail; } > /var/run/motd'",
+        refreshonly => true,
+        subscribe   => Class["debian::motd::files"]
+    }
+}
+
+class debian::motd {
+
+    $greeting_before_fqdn = $debian_motd_greeting_before_fqdn
+    $lead = $debian_motd_lead
+    $headeritems = $debian_motd_headeritems ? {
+        ""      => [],
+        default => $debian_motd_headeritems
+    }
+    $newsitems = $debian_motd_newsitems ? {
+        ""      => [],
+        default => $debian_motd_newsitems
+    }
+
+    include debian::motd::files, debian::motd::update
+}
+
+# vim:ft=puppet
