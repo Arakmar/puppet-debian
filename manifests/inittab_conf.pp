f27a2184c863b152b69a718ee64e6b5be6575fb4
Yoann Laissus <yoann.laissus@gmail.com>
2012-05-22 12:29:31 +0200

    Correct file name and prefix

---
+++ manifests/inittab_conf.pp
@@ -0,0 +1,21 @@
+#
+# This resource manages the /etc/inittab file
+# It's possible to add new config entries
+#
+
+define debian::inittab_conf (
+                $id,
+                $runlevels = '123456',
+                $action = 'respawn',
+                $process
+        ) 
+{
+	augeas {"inittab_$id":
+		context => "/files/etc/inittab",
+		changes => [
+			"set $id/runlevels $runlevels",
+			"set $id/action $action",
+			"set $id/process '$process'",
+			],
+	}
+}
