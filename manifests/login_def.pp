ef7d2051a60971d28b3ad2bd63bea428a215b6c7
Yoann Laissus <yoann.laissus@gmail.com>
2012-06-19 11:37:18 +0200

    Add a class to manage the /etc/login.def file For now, it only handles the umask value

---
+++ manifests/login_def.pp
@@ -0,0 +1,15 @@
+ class debian::login_def {
+
+    $umask = $debian_umask ? {
+        ""      => 022,
+        default => $debian_umask
+    }
+
+    file { "/etc/login.defs":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        content => template("debian/login.defs.erb")
+    }
+}
