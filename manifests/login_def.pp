6dd3fa7b8bf074f1d94dbaef0af8f1fa5e7cd8bf
Thomas Pierson <thomas@mezcalito.fr>
2013-08-23 12:16:34 +0200

    reformat login_def manifest and set umask param as a string

--- manifests/login_def.pp
+++ manifests/login_def.pp
@@ -1,12 +1,12 @@
 class debian::login_def(
-    $umask = 022
+    $umask = '022',
 ) {
 
-    file { "/etc/login.defs":
-        ensure  => file,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        content => template("debian/login.defs.erb")
-    }
+	file { "/etc/login.defs":
+	    ensure  => file,
+	    owner   => root,
+	    group   => root,
+	    mode    => 644,
+	    content => template("debian/login.defs.erb")
+	}
 }
